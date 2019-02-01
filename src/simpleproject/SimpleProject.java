999289f356a03aca680054b2a79e5619e646602c
ru2@students.ptcollege.edu <>
2019-02-01 09:47:36 -0500

    First Commit

---
+++ src/simpleproject/SimpleProject.java
@@ -0,0 +1,17 @@
+
+package simpleproject;
+
+
+public class SimpleProject {
+
+   
+    public static void main(String[] args) {
+       Urbassik();
+       
+    }
+    public static void Urbassik()
+    {
+        System.out.println("Ray Urbassik");
+    }
+    
+}
